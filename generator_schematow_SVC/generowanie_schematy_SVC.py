0a4416453067e61634259de919f901ad30723f00
Marcin <mfmarcinfrontczak@gmail.com>
2025-05-16 12:48:13 +0200

    prace na czytelnoscia kodu, oraz wstepne prace nad dodaniem generowania kolejnych stopni

--- generator_schematow_SVC/generowanie_schematy_SVC.py
+++ generator_schematow_SVC/generowanie_schematy_SVC.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 from PIL import Image
 
-# poniżej są domyslne radio przyciski -
+#*****************************************************************************************
+#BLOK 1
+# w tym bloku są zmienne
+# poniżej są zmienne dla funkcji pobierz_dane() - oraz dla ustawienia wartości początkowych - czyli tych które są
+#zaznaczone zaraz po uruchomieniu programu - przypisanie tych wartości do przycisków w funkcji tk.StringVar(value="nazwa zmiennej")
 zab_zew = "typ S - C 3polowe"
 podzespoly_dodatkowe = "tylko łącznik tyrystorowy i 3 dlawiki"
 zabezpieczenie_lacznika = "lacznik i stopnie - wspolne zabezpieczenie"
@@ -18,24 +22,31 @@ s12 = ""
 s13 = ""
 s14 = ""
 s15 = ""
-dodatkowe_stopnie = ["", "", "", "", "", "", "", "", "", "", "", "", ""]
+dodatkowe_stopnie = ["", "", "", "", "", "", "", "", "", "", "", ""]
 
-# Tworzymy główne okno
+#******************************************************************************************
+#BLOK 2
+# Tworzymy główne okno - narazie puste , w kolejnych blokach dodajemy elementy
 root = tk.Tk()
 root.title("Wybór opcji")
 
+#******************************************************************************************
+#BLOK 3
+#tu blok kodu odpowiedzialny za pierwsze przyciski radiowe i obsługę tych przycisków ( wybranie rodzaju zabezpieczenia zew. inaczej glównego
+#Wyświetlamy informacje nad pierwszymi przyciskami radiowymi
 zabezpieczenia_zew = tk.Label(text="Wybierz rodzaj zabezpieczenia zewnetrznego")
 zabezpieczenia_zew.grid(row=0, column=0)
 
-# Zmienna do przechowywania wybranej wartości - gdzie value to wartość początkowa - może się zmienić podczas pracy z GUI
-wybor = tk.StringVar(value="typ S - C 3polowe")  # Domyślnie wybrana opcja
-
-# Funkcja wywoływana po zmianie wyboru
+#Funkcja do obslugi przyciskow - jest przed deklaracją przyciskow - aby ją widziały
+#będzie wywoływana przez wybranie jedengo z przycisków
+# Funkcja wywoływana po zmianie wyboru lub kliknięciu na juz zaznaczony
 def pokaz_wybor():
     global zab_zew
     print("Wybrano:", wybor.get())
-    zab_zew = wybor.get() #
+    zab_zew = wybor.get() # do zmiennej przypisujemy wartosc wybraną -
 
+# Zmienna do przechowywania wybranej wartości - gdzie value to wartość początkowa - może się zmienić podczas pracy z GUI
+wybor = tk.StringVar(value=zab_zew)  # Domyślnie wybrana opcja, zmienna zab_zew jest na początku skryptu
 
 # Tworzenie przycisków radiowych
 opcje = ["typ S - C 3polowe", "wkladki topikowe gG", "brak zabezpieczenia glownego"]
@@ -45,17 +56,18 @@ for opcja in opcje:
     tk.Radiobutton(root, text=opcja, variable=wybor, value=opcja, command=pokaz_wybor).grid(row=i , column=0)
     i = i+1
 
-# fragment ywswietlanego tekstu
+#tu koniec bloku odpowiedzialnego za pierwsze przyciski radiowe - odnosnie zabezpieczenia zew. - głównego
+
+#********************************************************************************************
+#BLOK 4
+#tu blok  odpowiedzialny za przyciski radiowe - odnosnie - czy są dodatkowe stopnie
+#wyświetlenie pierwszego tekstu
 lacznik_tyrystorowy_i_stopnie = tk.Label(text="Ilosc podzespolow")
 lacznik_tyrystorowy_i_stopnie.grid(row=4, column=0) #gdzie umieszczone
 
-
-# Zmienna do przechowywania wybranej wartości - gdzie value to wartość początkowa - może się zmienić podczas pracy z GUI
-wybor3 = tk.StringVar(value="lacznik i stopnie - wspolne zabezpieczenie")
-opcje3 = ["lacznik tyrystorowy ma swoje zabezpieczenie", "lacznik i stopnie - wspolne zabezpieczenie"]
-# Przechowywanie dynamicznie tworzonych widgetów
-radio_buttons = []
-miejsce_podlaczenia_zab = None
+#funkcja do obsługi wybranego przyciski - funkcja musi być przed deklaracją przyciskow,
+#działanie jeżeli zostanie wybrany przycisk - "dodatkowe stopnie" to funkcja ma umiejscowić w oknie
+#dodatkowe pola na wpisanie wartości - dodatkowych stopni - definicje dodatkowych elementow są w innym BLOKU
 
 def pokaz_wybor2():
     global miejsce_podlaczenia_zab, radio_buttons, podzespoly_dodatkowe
@@ -131,8 +143,10 @@ def pokaz_wybor2():
         stopien14.grid(row=19, column=4)
         stopien15.grid(row=19, column=5)
 
+
+
 # Zmienna do przechowywania wybranej wartości - gdzie value to wartość początkowa - może się zmienić podczas pracy z GUI
-wybor2 = tk.StringVar(value="tylko łącznik tyrystorowy i 3 dlawiki")
+wybor2 = tk.StringVar(value=podzespoly_dodatkowe)
 opcje2 = ["tylko łącznik tyrystorowy i 3 dlawiki", "dodatkowe stopnie"]
 i = 5
 for opcja2 in opcje2:
@@ -140,11 +154,29 @@ for opcja2 in opcje2:
     tk.Radiobutton(root, text=opcja2, variable=wybor2, value=opcja2, command=pokaz_wybor2).grid(row=i , column=0)
     i = i+1
 
+
+#**********************************************************************************************
+#BLOK 5
+# trzecia częśc przyciskow radiowych - dostepna jezeli wybiorę opcje - dodatkowe stopnie
+# Zmienna do przechowywania wybranej wartości - gdzie value to wartość początkowa - może się zmienić podczas pracy z GUI
+wybor3 = tk.StringVar(value=zabezpieczenie_lacznika)
+opcje3 = ["lacznik tyrystorowy ma swoje zabezpieczenie", "lacznik i stopnie - wspolne zabezpieczenie"]
+# Przechowywanie dynamicznie tworzonych widgetów
+radio_buttons = []
+miejsce_podlaczenia_zab = None
+
+
+
+
+#**********************************************************************************************
+#BLOK 6
+#
 miejsce_podlaczenia_zab = tk.Label(text="Wybierz sposub podlaczenia zabezpieczenia lacznika tyrystorowego")
 
 opis_dlawiki_lacznik = tk.Label(root, text="podaj warosci trzech dlawikow podpietych do lacznika tyrystorowe")
 opis_dlawiki_lacznik .grid(row=10,column=0)
 
+#pola do wpisywania wartości dla trzech dławików - tych co są zawsze , podpięte do łącznika tyrystorowego
 dlawik1 = tk.Entry(root, width=30)
 dlawik1.grid(row=11,column=0)
 dlawik2= tk.Entry(root, width=30)
@@ -152,6 +184,7 @@ dlawik2.grid(row=12,column=0)
 dlawik3= tk.Entry(root, width=30)
 dlawik3.grid(row=13,column=0)
 
+#dodatkowe_stopnie - wyświetlane jeżeli zostanie wykonana funkcja
 dodatkowe_stopnie_jednfazowe = tk.Label(root, text="Podaj wartosci stopni jednofazowych")
 stopien4= tk.Entry(root, width=30)
 stopien5= tk.Entry(root, width=30)
@@ -175,51 +208,52 @@ def pobierz_dane():
     zabezpieczenie_lacznika=wybor3.get()
     print(zabezpieczenie_lacznika)
     d1 = dlawik1.get()
-    print(d1)
+    print(f"d1 = {d1}")
     d2 = dlawik2.get()
-    print(d2)
+    print(f"d2 = {d2}")
     d3 = dlawik3.get()
-    print(d3)
+    print(f"d3 = {d3}")
     s4 = stopien4.get()
-    print(s4)
+    print(f"s4 = {s4}")
     dodatkowe_stopnie[0] = s4
     s5 = stopien5.get()
-    print(s5)
+    print(f"s5 = {s5}")
     dodatkowe_stopnie[1] = s5
     s6 = stopien6.get()
-    print(s6)
+    print(f"s6 = {s6}")
     dodatkowe_stopnie[2] = s6
     s7 = stopien7.get()
-    print(s7)
+    print(f"s7 = {s7}")
     dodatkowe_stopnie[3] = s7
     s8 = stopien8.get()
-    print(s8)
+    print(f"s8 = {s8}")
     dodatkowe_stopnie[4] = s8
     s9 = stopien9.get()
-    print(s9)
+    print(f"s9 = {s9}")
     dodatkowe_stopnie[5] = s9
     s10 = stopien10.get()
-    print(s10)
+    print(f"s10 = {s10}")
     dodatkowe_stopnie[6] = s10
     s11 = stopien11.get()
-    print(s11)
+    print(f"s11 = {s11}")
     dodatkowe_stopnie[7] = s11
     s12 = stopien12.get()
-    print(s12)
+    print(f"s12 = {s12}")
     dodatkowe_stopnie[8] = s12
     s13 = stopien13.get()
-    print(s13)
+    print(f"s13 = {s13}")
     dodatkowe_stopnie[9] = s13
     s14 = stopien14.get()
-    print(s14)
+    print(f"s14 = {s14}")
     dodatkowe_stopnie[10] = s14
     s15 = stopien15.get()
-    print(s15)
+    print(f"s15 = {s15}")
     dodatkowe_stopnie[11] = s15
 
-    #tu do poprawy 
+    #to jest podzielone na czesc
     # ta czesc odpowiedzialna za schematy z termostatem - badz bez , oraz za schematy bez dodatkowych stopni
     if zab_zew == "typ S - C 3polowe" and podzespoly_dodatkowe == "tylko łącznik tyrystorowy i 3 dlawiki":
+        print("jestemy w if ")
         schemat_podstawowy = Image.open("pod_3_stopnie_uniwersalny/SVC_3_stopnie.png")
         width_podstawowy, height_podstawowy = schemat_podstawowy.size
         obraz_do_wygenerowania = Image.new("RGBA", (width_podstawowy, height_podstawowy))
@@ -227,6 +261,7 @@ def pobierz_dane():
         obraz_do_wygenerowania.save(f"wygenerowany.png")
 
     if zab_zew == "wkladki topikowe gG" and podzespoly_dodatkowe == "tylko łącznik tyrystorowy i 3 dlawiki":
+        print("jestemy w if ")
         schemat_podstawowy = Image.open("pod_3_stopnie_uniwersalny/SVC_3_stopnie_termostat_zabezpieczenie_zew_gG.png")
         width_podstawowy, height_podstawowy = schemat_podstawowy.size
         obraz_do_wygenerowania = Image.new("RGBA", (width_podstawowy, height_podstawowy))
@@ -234,6 +269,7 @@ def pobierz_dane():
         obraz_do_wygenerowania.save(f"wygenerowany.png")
 
     if zab_zew == "brak zabezpieczenia glownego" and podzespoly_dodatkowe == "tylko łącznik tyrystorowy i 3 dlawiki":
+        print("jestemy w if ")
         schemat_podstawowy = Image.open("pod_3_stopnie_uniwersalny/SVC_3_stopnie_termostat.png")
         width_podstawowy, height_podstawowy = schemat_podstawowy.size
         obraz_do_wygenerowania = Image.new("RGBA", (width_podstawowy, height_podstawowy))
@@ -266,12 +302,22 @@ def pobierz_dane():
             if ilosc_stopni == 6:
                 schemat_podstawowy = Image.open("pod_3_stopnie_uniwersalny/schematy_dodatkowe_stopnie/SVC_3_termostat_zew_gG_6stopnie_C_wszystkie_stopnie.png")
                 width_podstawowy, height_podstawowy = schemat_podstawowy.size
+
             siec_i_3_fazy = Image.open("pod_3_stopnie_uniwersalny/schematy_dodatkowe_stopnie/podlaczenie_stopnie/podlaczenie_glowne_i_siec.png")
             width_sieci_i_fazy, height_sieci_i_fazy = siec_i_3_fazy.size
 
             obraz_do_wygenerowania = Image.new("RGBA", (width_podstawowy, height_podstawowy + height_sieci_i_fazy))
             obraz_do_wygenerowania.paste(schemat_podstawowy,(0,0))
             obraz_do_wygenerowania.paste(siec_i_3_fazy, (0, height_podstawowy))
+            for i in range(len(dodatkowe_stopnie)): #liczymy ile jest elementow w tablicy
+                zawartosc = dodatkowe_stopnie[i]
+                if zawartosc== "":
+                    print("brak stopnia")
+                elif zawartosc.startswith("-"):
+                    print("dławik")
+                    obraz_do_wygenerowania = Image.open("pod_3_stopnie_uniwersalny/schematy_dodatkowe_stopnie/SVC_3_termostat_zew_gG_6stopnie_C_wszystkie_stopnie.png")
+                else:
+                    print("kondensator")
 
 
             obraz_do_wygenerowania.save(f"wygenerowany.png")
